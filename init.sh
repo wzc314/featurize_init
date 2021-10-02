a6337046fbff7567318a4b4af1e6ecfe2d466792
Zicong Wang <wzc314@qq.com>
2021-10-02 23:00:47 +0800

    Update config files.

--- init.sh
+++ init.sh
@@ -2,3 +2,6 @@
 
 cp .vimrc .condarc .tmux.conf ~
 conda clean -i
+
+ln -s ~/work/envs/pruning /environment/python/versions/miniconda3-4.7.12/envs/
+ln -s /environment/python/versions/miniconda3-4.7.12/bin/activate ~/work/.local/bin
