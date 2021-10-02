45aa03fd8fb69227327b51e06bdb4fd076ac81f0
Zicong Wang <wzc314@qq.com>
2021-10-02 23:11:51 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cp .vimrc .condarc .tmux.conf ~
-conda clean -i
+ln -sf ~/work/projects/featurize_init/{.vimrc,.condarc,.tmux.conf} ~ && conda clean -i
 
-ln -s ~/work/envs/pruning /environment/python/versions/miniconda3-4.7.12/envs/
-ln -s /environment/python/versions/miniconda3-4.7.12/bin/activate ~/work/.local/bin
+ln -sf ~/work/envs/pruning /environment/python/versions/miniconda3-4.7.12/envs/
+ln -sf /environment/python/versions/miniconda3-4.7.12/bin/activate ~/work/.local/bin
